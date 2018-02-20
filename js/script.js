0ef88c5edb4b1a7afa88215ff48971c490d5fc00
Dmitriy Sidorov <DmitriyWhite@icloud.com>
2018-02-20 19:33:46 +0300

    function delete, edit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var inputNewTask = document.querySelector('#new-task');
-var addButton = document.querySelector('#add');
-var inputUnfinishedTask = document.querySelector('#unfinished-tasks');
-var inputFinishedTask = document.querySelector('#finished-tasks');
+var inputNewTask = document.querySelector('#new-task'),
+    addButton = document.querySelector('#add'),
+    inputUnfinishedTask = document.querySelector('#unfinished-tasks'),
+    inputFinishedTask = document.querySelector('#finished-tasks');
 
 function createNewElement(task) {
     var listItem = document.createElement('li'),
@@ -23,8 +23,8 @@ function createNewElement(task) {
     listItem.appendChild(checkbox);
     listItem.appendChild(label);
     listItem.appendChild(input);
-    listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
+    listItem.appendChild(editButton);
 
     return listItem;
 }
@@ -33,7 +33,48 @@ function addTask() {
     if (inputNewTask.value) {
         var listItem = createNewElement(inputNewTask.value);
         inputUnfinishedTask.appendChild(listItem);
+        bindTaskEvents(listItem, finishTask);
         inputNewTask.value = "";
     }
 }
+
 addButton.onclick = addTask;
+
+function deleteTask() {
+    var listItem = this.parentNode,
+        itemRemove = listItem.parentNode;
+    itemRemove.removeChild(listItem);
+}
+
+function editTask() {
+    var editButton = this,
+        listItem = this.parentNode,
+        label = listItem.querySelector('label'),
+        input = listItem.querySelector('input[type=text]'),
+        containsClass = listItem.classList.contains('editMode');
+
+    if (containsClass) {
+        label.innerText = input.value;
+        editButton.className = 'material-icons edit';
+        editButton.innerHTML = '<i class="material-icons">edit</i>';
+    } else {
+        input.value = label.innerText;
+        editButton.className = 'material-icons save';
+        editButton.innerHTML = '<i class="material-icons">save</i>';
+    }
+    listItem.classList.toggle('editMode');
+}
+
+function finishTask() {
+    console.log('перенос в завершенные задачи')
+}
+
+function bindTaskEvents(listItem, checkboxEvent) {
+    var checkbox = listItem.querySelector('button.checkbox');
+    var editButton = listItem.querySelector('button.edit');
+    var deleteButton = listItem.querySelector('button.delete');
+
+    checkbox.onclick = checkboxEvent;
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+}
